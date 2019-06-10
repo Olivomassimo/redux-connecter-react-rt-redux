bd679714012937efcf57c43b4d85271b62a12c7a
massimo <massimo.olivo@gmail.com>
2019-06-10 17:12:32 +0200

    fin

---
+++ src/CounterContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+const mapStateToProps = state => ({
+  counter: state
+});
+
+const CounterComponent = ({ counter, dispatch }) => (
+  <div>
+    <p>{counter}</p>
+    <button onClick={() => dispatch({ type: 'ADD' })}>Add</button>
+    <button onClick={() => dispatch({ type: 'ADD10' })}>Add 10</button>
+    <button onClick={() => dispatch({ type: 'REMOVE' })}>Remove</button>
+    <button onClick={() => dispatch({ type: 'REMOVE10' })}>Remove 10</button>
+    <button onClick={() => dispatch({ type: 'RESET' })}>Reset</button>
+  </div>
+);
+
+
+
+export default connect(mapStateToProps)(CounterComponent);
